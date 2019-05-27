480962a21956dbe197f11d5b013453b08bee1f4f
Corentin <corentin.doco@phelma.grenoble-inp.fr>
2019-05-27 08:17:44 +0200

    test work well, reduce cube size

--- tests/test_datalabeler_mlar.py
+++ tests/test_datalabeler_mlar.py
@@ -18,10 +18,13 @@ def test_dataLabeler_mlar(example_dataset_adi):
 
     radius_int = dataset.cube.shape[2]/2
 
+    if dataset.cube.shape[0] > 80:
+        dataset.cube = dataset.cube[0:80]
+
     try:
         labeler = DataLabeler('mlar', dataset.cube, dataset.angles,
                               dataset.psf, radius_int=radius_int,
-                              fwhm=dataset.fwhm, plsc=float(dataset.plsc[0]),
+                              fwhm=dataset.fwhm, plsc=0.01,
                               min_snr=3, max_snr=5, n_proc=2)
     except TypeError:
         raise
