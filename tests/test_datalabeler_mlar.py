5018e1506854a982291e9da36d384844fb61d25e
Corentin <corentin.doco@phelma.grenoble-inp.fr>
2019-05-27 08:22:09 +0200

    add some print

--- tests/test_datalabeler_mlar.py
+++ tests/test_datalabeler_mlar.py
@@ -18,13 +18,15 @@ def test_dataLabeler_mlar(example_dataset_adi):
 
     radius_int = dataset.cube.shape[2]/2
 
+    print("distances taken at min radius : {]".format(radius_int))
+
     if dataset.cube.shape[0] > 80:
         dataset.cube = dataset.cube[0:80]
 
     try:
         labeler = DataLabeler('mlar', dataset.cube, dataset.angles,
                               dataset.psf, radius_int=radius_int,
-                              fwhm=dataset.fwhm, plsc=0.01,
+                              fwhm=dataset.fwhm, plsc=0.02719,
                               min_snr=3, max_snr=5, n_proc=2)
     except TypeError:
         raise
