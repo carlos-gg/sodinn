510566c10c2ecaa483d0055c6e29ab4d45939d44
Carlos Gomez <carlosgg33@gmail.com>
2019-05-29 11:59:15 +0200

    updating __init__

--- sodinn/__init__.py
+++ sodinn/__init__.py
@@ -1,6 +1,10 @@
 
 __version__ = "0.1"
 
+from . import data_labeling
+from . import prediction
+from . import models
+
 from .data_labeling import DataLabeler
 from .prediction.prediction import Predictor
 from .models.models import Model
