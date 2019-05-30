46113bdf9910a85a559d122afd9cbb4c562aa741
Corentin <corentin.doco@phelma.grenoble-inp.fr>
2019-05-30 13:07:20 +0200

    Update test to keep labeler in memory

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,11 @@ Configuration file for pytest, containing global ("session-level") fixtures.
 
 """
 
+import copy
 from pytest import fixture
 from astropy.utils.data import download_file
 import vip_hci as vip
+from sodinn import DataLabeler
 
 
 @fixture(scope="session")
@@ -49,3 +51,113 @@ def example_dataset_adi():
     dataset.psf = dataset.psfn
 
     return dataset
+
+
+@fixture(scope="session")
+def dataLabeler_mlar(example_dataset_adi):
+    """
+    Parameters
+    ----------
+    example_dataset_adi : fixture
+        Taken automatically from ``conftest.py``.
+    """
+
+    dataset = copy.copy(example_dataset_adi)
+
+    labeler_mlar = dataLabeler_type_test(dataset, "mlar")
+
+    return labeler_mlar
+
+
+@fixture(scope="session")
+def dataLabeler_tmlar(example_dataset_adi):
+    """
+    Parameters
+    ----------
+    example_dataset_adi : fixture
+        Taken automatically from ``conftest.py``.
+    """
+
+    dataset = copy.copy(example_dataset_adi)
+
+    labeler_tmlar = dataLabeler_type_test(dataset, "tmlar")
+
+    return labeler_tmlar
+
+
+@fixture(scope="session")
+def dataLabeler_tmlar4d(example_dataset_adi):
+    """
+    Parameters
+    ----------
+    example_dataset_adi : fixture
+        Taken automatically from ``conftest.py``.
+    """
+
+    dataset = copy.copy(example_dataset_adi)
+
+    labeler_tmlar4d = dataLabeler_type_test(dataset, "tmlar4d")
+
+    return labeler_tmlar4d
+
+
+@fixture(scope="session")
+def dataLabeler_pw3d(example_dataset_adi):
+    """
+    Parameters
+    ----------
+    example_dataset_adi : fixture
+        Taken automatically from ``conftest.py``.
+    """
+
+    dataset = copy.copy(example_dataset_adi)
+
+    labeler_pw3d = dataLabeler_type_test(dataset, "pw3d")
+
+    return labeler_pw3d
+
+
+@fixture(scope="session")
+def dataLabeler_pw2d(example_dataset_adi):
+    """
+    Parameters
+    ----------
+    example_dataset_adi : fixture
+        Taken automatically from ``conftest.py``.
+    """
+
+    dataset = copy.copy(example_dataset_adi)
+
+    labeler_pw2d = dataLabeler_type_test(dataset, "pw2d")
+
+    return labeler_pw2d
+
+
+def dataLabeler_type_test(dataset, sample_type):
+
+    radius_int = round(dataset.fwhm * 2)
+
+    try:
+        labeler = DataLabeler(sample_type, dataset.cube, dataset.angles,
+                              dataset.psf, radius_int=radius_int,
+                              fwhm=dataset.fwhm, plsc=0.02719,
+                              min_snr=3, max_snr=5, n_proc=2)
+    except TypeError:
+        raise
+
+    try:
+        labeler.estimate_fluxes(plot=False)
+    except TypeError:
+        raise
+
+    try:
+        labeler.run()
+    except TypeError:
+        raise
+
+    try:
+        labeler.augment()
+    except TypeError:
+        raise
+
+    return labeler
--- tests/test_datalabeler.py
+++ tests/test_datalabeler.py
@@ -2,161 +2,52 @@
 Tests for DataLabeler using different sample type
 """
 
-import copy
-from sodinn.data_labeling.labeling import DataLabeler
-from vip_hci.preproc import frame_crop
 
-
-def test_dataLabeler_mlar(example_dataset_adi):
-    """
-    Parameters
-    ----------
-    example_dataset_adi : fixture
-        Taken automatically from ``conftest.py``.
-    """
-
-    dataset = copy.copy(example_dataset_adi)
-
-    if dataset.cube.shape[0] > 80:
-        dataset.cube = dataset.cube[0:80]
-        dataset.angles = dataset.angles[0:80]
-
-    psf_croped = frame_crop(dataset.psf, int(round(dataset.fwhm))*2+1,
-                            force=True, verbose=False)
+def test_dataLabeler_mlar(dataLabeler_mlar):
 
     try:
-        labeler_mlar = dataLabeler_type_test(dataset, "mlar")
+        labeler_mlar = dataLabeler_mlar
     except TypeError:
         raise
 
     return True
 
 
-def test_dataLabeler_tmlar(example_dataset_adi):
-    """
-    Parameters
-    ----------
-    example_dataset_adi : fixture
-        Taken automatically from ``conftest.py``.
-    """
-
-    dataset = copy.copy(example_dataset_adi)
-
-    if dataset.cube.shape[0] > 80:
-        dataset.cube = dataset.cube[0:80]
-        dataset.angles = dataset.angles[0:80]
-
-    psf_croped = frame_crop(dataset.psf, int(round(dataset.fwhm)) * 2 + 1,
-                            force=True, verbose=False)
+def test_dataLabeler_tmlar(dataLabeler_tmlar):
 
     try:
-        labeler_mlar = dataLabeler_type_test(dataset, "tmlar")
+        labeler_tmlar = dataLabeler_tmlar
     except TypeError:
         raise
 
     return True
 
 
-def test_dataLabeler_tmlar4d(example_dataset_adi):
-    """
-    Parameters
-    ----------
-    example_dataset_adi : fixture
-        Taken automatically from ``conftest.py``.
-    """
-
-    dataset = copy.copy(example_dataset_adi)
-
-    if dataset.cube.shape[0] > 80:
-        dataset.cube = dataset.cube[0:80]
-        dataset.angles = dataset.angles[0:80]
-
-    psf_croped = frame_crop(dataset.psf, int(round(dataset.fwhm)) * 2 + 1,
-                            force=True, verbose=False)
+def test_dataLabeler_tmlar4d(dataLabeler_tmlar4d):
 
     try:
-        labeler_mlar = dataLabeler_type_test(dataset, "tmlar4d")
+        labeler_tmlar4d = dataLabeler_tmlar4d
     except TypeError:
         raise
 
     return True
 
 
-def test_dataLabeler_pw3d(example_dataset_adi):
-    """
-    Parameters
-    ----------
-    example_dataset_adi : fixture
-        Taken automatically from ``conftest.py``.
-    """
-
-    dataset = copy.copy(example_dataset_adi)
-
-    if dataset.cube.shape[0] > 80:
-        dataset.cube = dataset.cube[0:80]
-        dataset.angles = dataset.angles[0:80]
-
-    psf_croped = frame_crop(dataset.psf, int(round(dataset.fwhm)) * 2 + 1,
-                            force=True, verbose=False)
+def test_dataLabeler_pw3d(dataLabeler_pw3d):
 
     try:
-        labeler_mlar = dataLabeler_type_test(dataset, "pw3d")
+        labeler_pw3d = dataLabeler_pw3d
     except TypeError:
         raise
 
     return True
 
 
-def test_dataLabeler_pw2d(example_dataset_adi):
-    """
-    Parameters
-    ----------
-    example_dataset_adi : fixture
-        Taken automatically from ``conftest.py``.
-    """
-
-    dataset = copy.copy(example_dataset_adi)
-
-    if dataset.cube.shape[0] > 80:
-        dataset.cube = dataset.cube[0:80]
-        dataset.angles = dataset.angles[0:80]
-
-    psf_croped = frame_crop(dataset.psf, int(round(dataset.fwhm)) * 2 + 1,
-                            force=True, verbose=False)
+def test_dataLabeler_pw2d(dataLabeler_pw2d):
 
     try:
-        labeler_mlar = dataLabeler_type_test(dataset, "pw2d")
+        labeler_pw2d = dataLabeler_pw2d
     except TypeError:
         raise
 
     return True
-
-
-def dataLabeler_type_test(dataset, sample_type):
-
-    radius_int = round(dataset.fwhm * 2)
-
-    try:
-        labeler = DataLabeler(sample_type, dataset.cube, dataset.angles,
-                              dataset.psf, radius_int=radius_int,
-                              fwhm=dataset.fwhm, plsc=0.02719,
-                              min_snr=3, max_snr=5, n_proc=2)
-    except TypeError:
-        raise
-
-    try:
-        labeler.estimate_fluxes(plot=False)
-    except TypeError:
-        raise
-
-    try:
-        labeler.run()
-    except TypeError:
-        raise
-
-    try:
-        labeler.augment()
-    except TypeError:
-        raise
-
-    return labeler
